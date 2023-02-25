00ae225ca9bfb8c181cd717da91ea342934d1179
melm <melm248@yahoo.co.jp>
2023-02-25 20:29:26 +0900

    images追加

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
