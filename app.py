5b5530e7f6601173a8cf7ebbdc15b919ec4e6543
melm <melm248@yahoo.co.jp>
2023-03-05 14:01:48 +0900

    index()POST機能追加

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, flash, redirect, render_template, request, session, request, jsonify
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -16,3 +16,16 @@ def index():
             colors.append({'color_name': row[0], 'color_code': row[1]})
         c.close()
         return render_template("index.html", colors=colors)
+    else:
+        selected_color_code = request.form.get("selected_color_code")
+        conn = sqlite3.connect('color.db')
+        c = conn.cursor()
+        c.execute('SELECT matching_color_name, matching_color_code FROM color_matches WHERE color_code = ?', (selected_color_code,))
+        matches_colors = []
+        for row in c.fetchall():
+            matches_colors.append({'matching_color_name': row[0], 'matching_color_code': row[1]})
+        c.close()
+        return render_template("selected.html", matches_colors=matches_colors)
+
+if __name__ == '__main__':
+    app.run(debug=True)
